90ef371237b5febf79d0ba30eedf31a36133c937
marok39 <rok.marinsek94@gmail.com>
2016-04-06 14:26:58 +0000

    Zakljucek naloge

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ if (!process.env.PORT)
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('chinook.sl3');
 
+var perpage = 20;
+
 /* calls callback with specified page's artists and artist's details */
 var artists = function(page, artist, details, callback) {
   db.all("SELECT Artist.ArtistId, Name, StarsNo " +
     "FROM Artist, Stars WHERE Artist.ArtistId = Stars.ArtistId " +
-    "ORDER BY Name LIMIT 33 OFFSET ($page - 1) * 33",
+    "ORDER BY Name LIMIT " + perpage + " OFFSET ($page - 1) * " + perpage + "",
     {$page: page}, function(error, rows) {
       if (error) {
         console.log(error);
@@ -18,7 +20,7 @@ var artists = function(page, artist, details, callback) {
         var result = '<div id="artists">';
         for (var i = 0; i < rows.length; i++) {
           var selected = rows[i].ArtistId == artist;
-          result += '<div id="' + rows[i].ArtistId + '"><span class="numbers">' + (page * 33 + i - 32) + '.</span>' +
+          result += '<div id="' + rows[i].ArtistId + '"><span class="numbers">' + (page * perpage + i - (perpage-1)) + '.</span>' +
             '<a href="/artists/' + page + (!selected? '/details/' + rows[i].ArtistId: '') + '#' + rows[i].ArtistId + '">' +
             '<button type="button" class="btn btn-default' + (selected? ' selected': '') + '">' +
             rows[i].Name + '</button></a><span class="stars">';
@@ -93,9 +95,22 @@ var genres = function(artist, callback) {
         console.log(error);
         callback('<strong>Something went wrong!</strong>');
       } else {
-        var result = '<h5>Genres</h5><div id="genres">' + 
-          'No genres for this artist' + 
-          '</div>';
+        
+        if(rows.length > 0){
+          var result = ""; 
+          for(var i = 0; i<rows.length; i++) {
+            if(i==0) result += rows[i].Name;
+            else result += " | " + rows[i].Name;
+          }
+          result = '<h5>Genres</h5><div id="genres">' + 
+            result + 
+            '</div>';
+        }
+        else {
+          var result = '<h5>Genres</h5><div id="genres">' + 
+            'No genres for this artist' + 
+            '</div>';
+        }
         callback(result);
       }
   });
@@ -108,6 +123,10 @@ var app = express();
 /* settings for static application files */
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.redirect('/artists/1');
+});
 
 /* responds with first page's artists */
 app.get('/artists', function(request, response) {
@@ -187,8 +206,10 @@ app.get('/pages', function(request, response) {
       console.log(error);
       response.sendStatus(500);
     } else
-      response.send({pages: Math.ceil(row.Artists / 33)});
+      response.send({pages: Math.ceil(row.Artists / perpage)});
   });
 });
 
-
+app.listen(process.env.PORT, function() {
+    console.log("Streznik je zagnan.");       
+});
